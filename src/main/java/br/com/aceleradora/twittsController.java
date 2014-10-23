4ab9ee58d617f9e4cef33bbb56b96cf9abcaddf3
evertonjulian <evertonjulian@gmail.com>
2014-10-23 14:18:47 -0200

    primeiro commit

---
+++ src/main/java/br/com/aceleradora/twittsController.java
@@ -0,0 +1,11 @@
+package br.com.aceleradora;
+
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class twittsController {
+
+	public void olaMundo(){
+		System.out.println("Alguma mensagem");
+	}
+}
